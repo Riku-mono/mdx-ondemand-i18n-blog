0a480dfa111d0baa7c23201586a2eae77e4a1660
Riku-mono <riitkou@gmail.com>
2025-02-07 19:30:59 +0900

    Add admin page for dev

---
+++ src/app/[locale]/admin/Card.tsx
@@ -0,0 +1,125 @@
+/* eslint-disable */
+'use client';
+
+import { Post } from 'contentlayer/generated';
+import { useState } from 'react';
+
+interface PostCardProps {
+  post: Post;
+}
+
+interface LocaleCardProps {
+  locale: string;
+  isExists: boolean;
+  hasError: boolean;
+  data?: any; // 追加: データを格納する
+  type: 'post' | 'category'; // 追加：post or category
+}
+
+const PostCard = ({ post }: PostCardProps) => {
+  return (
+    <article className="flex flex-1 flex-col gap-2 rounded-xl p-4 hover:bg-gray-800">
+      <div className="flex flex-row items-center justify-between gap-2">
+        <h2 className="font-black">{post.title}</h2>{' '}
+        <span className="text-sm">{`/${post.slug}`}</span>
+      </div>
+      <footer>
+        <div>
+          <span>投稿: {post.date}</span>
+        </div>
+        {post.lastupdated && (
+          <div>
+            <span>最終更新: {post.lastupdated}</span>
+          </div>
+        )}
+      </footer>
+    </article>
+  );
+};
+
+// locale と 存在するかしないかと、問題があるかどうかを入力して表示する
+const LocaleCard = ({ locale, isExists, hasError, data, type }: LocaleCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  let displayContent;
+
+  if (isExists && hasError) {
+    displayContent = (
+      <div
+        className="flex cursor-pointer items-center rounded-md bg-yellow-300 px-2 py-1 text-black"
+        onClick={handleClick}
+      >
+        <span>⚠️</span>
+        <span>{locale}</span>
+      </div>
+    );
+  } else if (hasError) {
+    displayContent = (
+      <div
+        className="flex cursor-pointer items-center rounded-md bg-red-300 px-2 py-1 text-black"
+        onClick={handleClick}
+      >
+        <span>❌</span>
+        <span>{locale}</span>
+      </div>
+    );
+  } else if (!isExists) {
+    displayContent = (
+      <div
+        className="flex cursor-pointer items-center rounded-md bg-gray-500 px-2 py-1 text-black"
+        onClick={handleClick}
+      >
+        <span className="font-bold">❌</span>
+        <span>{locale}</span>
+      </div>
+    );
+  } else {
+    displayContent = (
+      <div
+        className="flex cursor-pointer items-center rounded-md bg-green-300 px-2 py-1 text-black"
+        onClick={handleClick}
+      >
+        <span>✅</span>
+        <span>{locale}</span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {displayContent}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 text-black">
+          <div className="relative">
+            <div
+              className="mt-20 max-w-3xl overflow-y-scroll rounded-md bg-white p-4"
+              style={{ maxHeight: '80vh' }}
+            >
+              <h2 className="mb-2 text-xl font-bold">
+                {locale} - {type === 'post' ? 'Post Data' : 'Category Data'}
+              </h2>
+              <pre className="whitespace-pre-wrap">{JSON.stringify(data, null, 2)}</pre>{' '}
+              {/* JSON を整形して表示 */}
+            </div>
+            <button
+              onClick={handleCloseModal}
+              className="absolute bottom-2 right-2 mt-4 rounded-md bg-gray-200 px-4 py-2 hover:bg-gray-300"
+            >
+              閉じる
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export { PostCard, LocaleCard };
---
+++ src/app/[locale]/admin/page.tsx
@@ -0,0 +1,178 @@
+// Blog 管理用のページ
+// development でのみ利用可能
+/* eslint-disable */
+
+import { CategoryCard } from '@/components/category/CategoryCard';
+import { defaultLocale, locales } from '@/i18n/i18nLocales';
+import { getCategoriesForLocale } from '@/lib/category';
+import { getPostsForLocale } from '@/lib/post';
+import { allCategories, allPosts, Post } from 'contentlayer/generated';
+import { notFound } from 'next/navigation';
+import { LocaleCard, PostCard } from './Card';
+
+interface PageProps {
+  params: Promise<{ locale: string }>;
+}
+
+const areArraysEqual = (arr1: any[], arr2: any[]) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const checkDataEquality = (data1: any, data2: any, keysToCheck: (keyof any)[]): boolean => {
+  return keysToCheck.some((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    const isEqual = Array.isArray(value1) ? areArraysEqual(value1, value2) : value1 === value2;
+
+    if (!isEqual) {
+      console.warn(key, value1, value2);
+    }
+    return !isEqual;
+  });
+};
+
+function H2({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl font-bold">{children}</h2>;
+}
+function H3({ children }: { children: React.ReactNode }) {
+  return <h3 className="border-b-2 pb-2 text-xl font-bold">{children}</h3>;
+}
+function Li({ children }: { children: React.ReactNode }) {
+  return <li className="mt-4 flex justify-between gap-2">{children}</li>;
+}
+
+export default async function AdminPage({ params }: PageProps) {
+  const { locale } = await params;
+  const displayNames = new Intl.DisplayNames([locale], { type: 'language' });
+
+  if (process.env.NODE_ENV !== 'development') {
+    return notFound();
+  }
+
+  const defaultLocalePosts = await getPostsForLocale(defaultLocale, true);
+  const defaultLocaleCategories = await getCategoriesForLocale(defaultLocale);
+
+  const defaultLocalePostsPublished = defaultLocalePosts.filter((post) => post.published);
+  const defaultLocalePostsUnpublished = defaultLocalePosts.filter((post) => !post.published);
+
+  const renderLocaleCards = (data: any, keysToCheck: (keyof any)[], type: 'post' | 'category') =>
+    locales.map((locale) => {
+      const dataForLocale =
+        type === 'post'
+          ? allPosts.find((p) => p.slug === data.slug && p.language === locale)
+          : allCategories.find((c) => c.slug === data.slug && c.language === locale);
+      const isExists = !!dataForLocale;
+      const hasError = isExists && checkDataEquality(data, dataForLocale, keysToCheck);
+
+      return (
+        <LocaleCard
+          key={locale}
+          locale={locale}
+          isExists={isExists}
+          hasError={hasError}
+          data={dataForLocale}
+          type={type}
+        />
+      );
+    });
+
+  const postKeysToCheck: Array<keyof Post> = [
+    'published',
+    'icon',
+    'date',
+    'lastupdated',
+    'categories',
+  ];
+
+  const categoryKeysToCheck: Array<keyof (typeof defaultLocaleCategories)[0]> = [
+    'className',
+    'icon',
+  ];
+
+  return (
+    <div className="mx-auto my-8 w-full max-w-screen-xl space-y-6 px-4 lg:px-10">
+      <h1 className="text-4xl font-black">管理用画面</h1>
+      <section>
+        <H2>ロケール一覧</H2>
+        <div className="flex flex-row gap-2">
+          {locales.map((locale) => (
+            <LocaleCard
+              key={locale}
+              locale={locale}
+              isExists={true}
+              hasError={false}
+              data={locale}
+              type={'post'}
+            />
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <H2>記事一覧</H2>
+        <p className="space-y-4 py-3 text-sm text-gray-300">
+          <span>{displayNames.of(defaultLocale)} の記事を元に、比較しています。</span>
+          <br />
+          <span>以下の properties の値が異なる場合は、エラーが表示されます:</span>
+          <span className="flex gap-2">
+            {postKeysToCheck.map((key) => (
+              <code key={key} className="rounded-md bg-gray-700 px-1 py-0.5 text-sm">
+                {key}
+              </code>
+            ))}
+          </span>
+        </p>
+        {/* emoji */}
+        <H3>公開中の記事🌐</H3>
+        <ul>
+          {defaultLocalePostsPublished.map((post) => (
+            <Li key={post.slug}>
+              <PostCard post={post} />
+              <div className="flex gap-2">{renderLocaleCards(post, postKeysToCheck, 'post')}</div>
+            </Li>
+          ))}
+        </ul>
+        <H3>非公開の記事🚧</H3>
+        <ul>
+          {defaultLocalePostsUnpublished.map((post) => (
+            <Li key={post.slug}>
+              <PostCard post={post} />
+              <div className="flex gap-2">{renderLocaleCards(post, postKeysToCheck, 'post')}</div>
+            </Li>
+          ))}
+        </ul>
+        <H3>不正な記事❌</H3>
+        <p className="text-sm text-gray-300">
+          {displayNames.of(defaultLocale)} が存在しないため、表示されません
+        </p>
+        <ul>
+          {allPosts
+            .filter((post) => !defaultLocalePosts.some((p) => p.slug === post.slug))
+            .map((post) => (
+              <Li key={post.slug}>
+                <PostCard post={post} />
+                <div className="flex gap-2">{renderLocaleCards(post, postKeysToCheck, 'post')}</div>
+              </Li>
+            ))}
+        </ul>
+      </section>
+      <section>
+        <H2>カテゴリ一覧</H2>
+        <ul>
+          {defaultLocaleCategories.map((category) => (
+            <Li key={category.slug}>
+              <CategoryCard slug={category.slug} locale={defaultLocale} />
+              <div className="flex gap-2">
+                {renderLocaleCards(category, categoryKeysToCheck, 'category')}
+              </div>
+            </Li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
