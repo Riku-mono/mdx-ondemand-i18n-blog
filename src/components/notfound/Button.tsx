b9753d9b96af1666654639340f9c81f7272e746c
Riku-mono <riitkou@gmail.com>
2025-02-10 01:44:10 +0900

    Add NotFound page and components with localization support

---
+++ src/components/notfound/Button.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+type ButtonProps = {
+  text: string;
+  href: string;
+  style?: string;
+  rel?: string;
+};
+
+const Button = ({ text, href }: ButtonProps) => {
+  return (
+    <Link
+      href={href}
+      target={href.startsWith('http') ? `_blank` : ''}
+      className="bg-primary-500 group relative inline-flex items-center justify-center overflow-hidden rounded-md px-4 py-1.5 text-xs font-normal transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg dark:bg-card"
+      style={{ textDecoration: 'none' }}
+    >
+      <span className="relative z-50 text-lg">{text}</span>
+      <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-13deg)_translateX(100%)]">
+        <div className="relative h-full w-8 bg-white/20" />
+      </div>
+    </Link>
+  );
+};
+
+export default Button;
