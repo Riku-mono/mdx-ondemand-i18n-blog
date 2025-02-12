d059a7615ea5e3008ad204c6e197ec62b442ba45
Riku-mono <riitkou@gmail.com>
2025-02-12 21:23:23 +0900

    Add default component: CustomeTabLayout

--- src/components/mdx/MDXComponents.tsx
+++ src/components/mdx/MDXComponents.tsx
@@ -2,9 +2,11 @@ import type { MDXComponents } from 'mdx/types';
 
 import TwoGridLayout from './elm/CustomTwoGridLayout';
 import Pre from './elm/CustomPre';
+import TabLayout from './elm/CustomTabLayout';
 
 // 事前に提供しているコンポーネント
 const defaultComponents = {
+  TabLayout,
   TwoGridLayout,
 };
 
---
+++ src/components/mdx/elm/CustomTabLayout.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { Children, useState } from 'react';
+
+// contents groupings layout
+
+interface CodeTabsProps {
+  children: React.ReactNode[];
+}
+
+interface TabProps {
+  children: React.ReactNode;
+  active: boolean;
+  onClick: () => void;
+}
+
+const Tab = ({ children, active, onClick }: TabProps) => (
+  <button
+    className={`rounded-full px-4 py-2 text-xs font-medium ${active ? 'bg-card text-primary' : 'text-muted'}`}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+const CodeTabs = ({ children }: CodeTabsProps) => {
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs = Children.map(children, (child) => {
+    if (typeof child === 'object' && child !== null && 'props' in child) {
+      const props = child.props as { title?: string };
+      if ('title' in props) {
+        return props.title;
+      }
+    }
+    return null;
+  });
+  const contents = Children.map(children, (child) => child);
+
+  return (
+    <div className="relative mt-2 rounded-md border border-border" data-tab-layout>
+      <div className="flex border-b border-border p-2">
+        {tabs?.map((tab, i) => (
+          <Tab key={i} active={activeTab === i} onClick={() => setActiveTab(i)}>
+            {tab}
+          </Tab>
+        ))}
+      </div>
+      {contents?.map((content, i) => (
+        <div key={i} className={`${activeTab === i ? 'block p-2' : 'hidden'}`}>
+          {content}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CodeTabs;
