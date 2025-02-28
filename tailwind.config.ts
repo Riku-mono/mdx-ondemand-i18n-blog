999b94311d7dd87ad917e7acc016c520a64b2c99
Riku-mono <riitkou@gmail.com>
2025-02-28 22:16:17 +0900

    Add Tailwind CSS icons plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 import typographyPlugin from '@tailwindcss/typography';
 import { PluginAPI } from 'tailwindcss/types/config';
+import iconsPlugin, { getIconCollections } from '@egoist/tailwindcss-icons';
 
 export default {
   darkMode: ['class'],
@@ -114,5 +115,16 @@ export default {
       }),
     },
   },
-  plugins: [typographyPlugin],
+  plugins: [
+    typographyPlugin,
+    iconsPlugin({
+      // Select the icon collections you want to use
+      // You can also ignore this option to automatically discover all individual icon packages you have installed
+      // If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
+      collections: getIconCollections(['lucide']),
+      // If you want to use all icons from @iconify/json, you can do this:
+      // collections: getIconCollections("all"),
+      // and the more recommended way is to use `dynamicIconsPlugin`, see below.
+    }),
+  ],
 } satisfies Config;
