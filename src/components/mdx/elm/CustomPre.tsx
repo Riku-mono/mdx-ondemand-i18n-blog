9b901c0276c10a12240d31dc265a91e5787dd4e7
Riku-mono <riitkou@gmail.com>
2025-02-28 23:26:27 +0900

    Update CustomPre style

--- src/components/mdx/elm/CustomPre.tsx
+++ src/components/mdx/elm/CustomPre.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Check, Clipboard } from 'lucide-react';
+import { Check, Copy } from 'lucide-react';
 import { DetailedHTMLProps, HTMLAttributes, useRef, useState } from 'react';
 
 export default function Pre({
@@ -29,13 +29,15 @@ export default function Pre({
       <button
         disabled={isCopied}
         onClick={handleClickCopy}
-        className="absolute right-2 top-2 z-10 size-8 rounded-md bg-card p-1 opacity-0 transition-opacity group-hover:opacity-100"
-        aria-label="Copy code"
+        className={`absolute right-2 top-2 z-10 rounded-md border border-border bg-background p-2 opacity-0 transition-opacity hover:bg-background-hover group-hover:opacity-100 ${isCopied ? 'cursor-not-allowed opacity-100' : ''}`}
+        title="Copy code"
       >
-        {isCopied ? <Check className="text-green-400" /> : <Clipboard />}
+        {isCopied ? <Check className="h-4 w-4 text-green-400" /> : <Copy className="h-4 w-4" />}
       </button>
       <pre ref={preRef} {...props} className={classNames}>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-4 bg-gradient-to-r from-card" />
         {children}
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-8 bg-gradient-to-l from-card" />
       </pre>
     </div>
   );
